d9f12efe0c24c4eeceb57831f3393afc7fa2b214
NguyenNhan8819 <phucnhan241088@gmail.com>
2025-11-07 14:40:19 +0700

    bai5 event

--- src/components/mycomponent.js
+++ src/components/mycomponent.js
@@ -1,13 +1,36 @@
 import react from "react";
 
 class MyComponent extends react.Component {
+  // let name = "My Component";
+  state = { name: "Nhân" };
+  handleOnChangeName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
   render() {
-    // let name = "My Component";
-    this.state = { name: "Nhân" };
     return (
-      <div>
-        <h1>This is {this.state.name}</h1>
-      </div>
+      <>
+        <div className="first">
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleOnChangeName}
+          />
+          <h1>This is {this.state.name}</h1>
+        </div>
+        <div className="second">
+          <h1>This is {this.state.name}</h1>
+        </div>
+        <div className="third">
+          <button
+            onClick={() => {
+              alert(`Hello ${this.state.name}`);
+            }}
+          >
+            Click me
+          </button>
+        </div>
+      </>
     );
   }
 }
