85e2f184de894437912f26f03e74f3a9fd5d5011
NguyenNhan8819 <phucnhan241088@gmail.com>
2025-11-07 13:43:46 +0700

    bai4 state

--- src/components/mycomponent.js
+++ src/components/mycomponent.js
@@ -2,9 +2,11 @@ import react from "react";
 
 class MyComponent extends react.Component {
   render() {
+    // let name = "My Component";
+    this.state = { name: "Nhân" };
     return (
       <div>
-        <h1>This is My Component</h1>
+        <h1>This is {this.state.name}</h1>
       </div>
     );
   }
